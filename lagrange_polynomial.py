9bc4de2f26fc2cb820e83bf74f96dc0c693a77db
tukitoki <tokichiihere@gmail.com>
2022-04-08 22:07:30 +0300

    add lagrange interpolation(not full work mb)

---
+++ lagrange_polynomial.py
@@ -0,0 +1,20 @@
+
+
+class LagrangePolynomial:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def calc_polynomial_at_x(self, x):
+        polynom = 0
+        for index in range(len(self.y)):
+            polynom += self.y[index] * self.__calc_basis_polynom(x, index)
+        return polynom
+
+    def __calc_basis_polynom(self, x, index):
+        polynom = 1
+        for x_index in range(len(self.x)):
+            if x_index != index:
+                polynom *= (x - self.x[x_index]) / (self.x[index] - self.x[x_index])
+        return polynom
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 from cubic_spline_2d import *
+from lagrange_polynomial import *
+from scipy.interpolate import lagrange
+import numpy as np
 
 
 def calculate_2d_spline_interpolation(x, y, num=100):
@@ -15,12 +18,24 @@ def calculate_2d_spline_interpolation(x, y, num=100):
     return result_x, result_y
 
 
+def calculate_2d_lagrange_polynom(x_points, y_points, num):
+    new_y_points = np.linspace(y_points[0], y_points[-1], num)
+    new_x_points = np.linspace(x_points[0], x_points[-1], num)
+    lagrange = LagrangePolynomial(x_points, y_points)
+    L = []
+    for x in new_x_points:
+        L.append(lagrange.calc_polynomial_at_x(x))
+    return new_x_points, L
+
+
 if __name__ == '__main__':
     x_points = []
     y_points = []
     fig, ax = plt.subplots(figsize=(9, 9), num="Cubic Splines Simple App")
+    np.seterr(over='raise')
 
     curve, = ax.plot(x_points, y_points, "-g", label="spline")
+    curve_lagrange, = ax.plot(x_points, y_points, "-r", label="lagrange")
     points, = ax.plot(x_points, y_points, "x")
 
     def on_click(event):
@@ -29,9 +44,12 @@ if __name__ == '__main__':
         y_points.append(y_new_point)
 
         if len(x_points) > 1 and len(x_points) == len(y_points):
-            x_curve_points, y_curve_points = calculate_2d_spline_interpolation(x_points, y_points, num=500)
+            x_curve_points, y_curve_points = calculate_2d_spline_interpolation(x_points, y_points, num=10000)
+            x_lagrange_curve_points, y_lagrange_curve_points = calculate_2d_lagrange_polynom(x_points, y_points, num=500)
             curve.set_xdata(x_curve_points)
             curve.set_ydata(y_curve_points)
+            curve_lagrange.set_xdata(x_lagrange_curve_points)
+            curve_lagrange.set_ydata(y_lagrange_curve_points)
 
         points.set_xdata(x_points)
         points.set_ydata(y_points)
